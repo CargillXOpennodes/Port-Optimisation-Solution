fc81f7f26267dca960f66255ce0fed24a402ad1e
Rebecca Chin <rebeccachinxy@gmail.com>
2020-11-07 21:30:08 +0800

    Update message.rs

--- examples/gameroom/daemon/src/rest_api/routes/message.rs
+++ examples/gameroom/daemon/src/rest_api/routes/message.rs
@@ -16,7 +16,7 @@ use std::collections::HashMap;
 use std::time::{Duration, SystemTime};
 
 use actix_web::{web, Error, HttpResponse};
-use gameroom_database::{helpers, models::Message, ConnectionPool};
+use gameroom_database::{helpers, models::Status, ConnectionPool};
 
 use crate::rest_api::RestApiResponseError;
 
@@ -25,64 +25,104 @@ use super::{
     DEFAULT_OFFSET,
 };
 
+
+// pub struct ApiMessage {
+//     id : i32,
+//     circuit_id: String,
+//     message_name: String,
+//     message_content: String,
+//     message_type: String,
+//     sender: String,
+//     previous_id: Option<i32>,
+//     participant_1: String,
+//     participant_2: String,
+//     created_time: u64,
+//     updated_time: u64,
+// }
+
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ApiMessage {
-    id : i32,
-    circuit_id: String,
-    message_name: String,
-    message_content: String,
-    message_type: String,
+pub struct ApiStatus {
+    name: String,
     sender: String,
-    previous_id: Option<i32>,
-    participant_1: String,
-    participant_2: String,
-    created_time: u64,
-    updated_time: u64,
+    participant1: String,
+    participant2: String,
+    participant1_short: String,
+    participant2_short: String,
+    docking_type: DockingType,
+    eta: Option<Duration>,
+    etb: Option<Duration>,
+    ata: Option<Duration>,
+    eto: Option<Duration>,
+    ato: Option<Duration>,
+    etc: Option<Duration>,
+    etd: Option<Duration>,
+    is_bunkering: Option<bool>,
+    bunkering_time: Option<Duration>,
+    logs: String,
 }
 
-impl From<Message> for ApiMessage {
-    fn from(msg: Message) -> Self {
-        Self {
-            id: msg.id,
-            circuit_id: msg.circuit_id.to_string(),
-            message_name: msg.message_name.to_string(),
-            message_content: msg.message_content.to_string(),
-            message_type: msg.message_type.to_string(),
-            sender: msg.sender.to_string(),
-            previous_id: msg.previous_id,
-            participant_1: msg.participant_1.to_string(),
-            participant_2: msg.participant_2.to_string(),
-            created_time: msg
-                .created_time
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap_or_else(|_| Duration::new(0, 0))
-                .as_secs(),
-            updated_time: msg
-                .updated_time
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap_or_else(|_| Duration::new(0, 0))
-                .as_secs(),
+// impl From<Message> for ApiMessage {
+//     fn from(sta: Message) -> Self {
+//         Self {
+//             id: sta.id,
+//             circuit_id: sta.circuit_id.to_string(),
+//             message_name: sta.message_name.to_string(),
+//             message_content: sta.message_content.to_string(),
+//             message_type: sta.message_type.to_string(),
+//             sender: sta.sender.to_string(),
+//             previous_id: sta.previous_id,
+//             participant_1: sta.participant_1.to_string(),
+//             participant_2: sta.participant_2.to_string(),
+//             created_time: sta
+//                 .created_time
+//                 .duration_since(SystemTime::UNIX_EPOCH)
+//                 .unwrap_or_else(|_| Duration::new(0, 0))
+//                 .as_secs(),
+//             updated_time: sta
+//                 .updated_time
+//                 .duration_since(SystemTime::UNIX_EPOCH)
+//                 .unwrap_or_else(|_| Duration::new(0, 0))
+//                 .as_secs(),
+//         }
+//     }
+// }
+
+impl From<Status> for ApiStatus {
+    fn from(sta: Status) -> Status {
+        Status {
+            name: name,
+            sender: sta.sender,
+            participant1: sta.participant1
+            participant2: sta.partcipant2
+            participant1_short: sta.participant1_short
+            participant2_short: sta.participant2_short
+            docking_type: sta.docking_type.DockingType(),
+            eta: sta.eta,
+            etb: sta.etb,
+            ata: sta.ata,
+            eto: sta.eto,
+            ato: sta.ato,
+            etc: sta.etc,
+            etd: sta.etd
+            is_bunkering: sta.is_bunkering
+            bunkering_time: sta.bunkering_time
+            logs: sta.logs
+
         }
     }
 }
 
-pub async fn list_messages(
-    pool: web::Data<ConnectionPool>,
-    circuit_id: web::Path<String>,
-    query: web::Query<HashMap<String, usize>>,
-) -> Result<HttpResponse, Error> {
-    let offset: usize = query
-        .get("offset")
-        .map(ToOwned::to_owned)
-        .unwrap_or_else(|| DEFAULT_OFFSET);
+pub async fn list_statuses(
+    statuses: HashMap<String, Status>
+) -> String {
+    let mut status_strings: Vec<String> = vec![];
+        for (_, status) in statuses {
+            status_strings.push(status.to_string().clone());
+        }
+        status_strings.sort();
+        status_strings.join("|")
 
-    let limit: usize = query
-        .get("limit")
-        .map(ToOwned::to_owned)
-        .unwrap_or_else(|| DEFAULT_LIMIT);
-    let base_link = format!("api/message/{}/messages?", &circuit_id);
-
-    match web::block(move || list_messages_from_db(pool, &circuit_id.clone(), limit, offset)).await
+    match web::block(move || list_statuses_from_db(pool, &circuit_id.clone(), limit, offset)).await
     {
         Ok((games, query_count)) => {
             let paging_info =
@@ -96,20 +136,20 @@ pub async fn list_messages(
     }
 }
 
-fn list_messages_from_db(
-    pool: web::Data<ConnectionPool>,
-    circuit_id: &str,
+
+
+fn list_statuses_from_db(
+    statuses: HashMap<String, Status>
     limit: usize,
     offset: usize,
-) -> Result<(Vec<ApiMessage>, i64), RestApiResponseError> {
+) -> Result<(Vec<ApiStatus>, i64), RestApiResponseError> {
     let db_limit = validate_limit(limit);
     let db_offset = offset as i64;
 
-    let messages = helpers::list_messages(&*pool.get()?, circuit_id, db_limit, db_offset)?
+    let statuses = helpers::list_statuses(&*statuses.get()?, circuit_id, db_limit, db_offset)?
         .into_iter()
-        .map(ApiMessage::from)
+        .map(ApiStatus::from)
         .collect();
-    let message_count = helpers::get_message_count(&*pool.get()?)?;
 
-    Ok((messages, message_count))
+    Ok((statuses))
 }
